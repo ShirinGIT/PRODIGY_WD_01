8e14f35c01d0d05bc6ae4985e857f276b3698261
SHIRIN FATHIMA A <118587633+ShirinGIT@users.noreply.github.com>
2024-07-17 19:58:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Add event listener to window scroll event
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
